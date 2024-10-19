f564cbae14e88a4d58977b0fda0d101b7ab5d309
eavlongs <eavlongs@gmail.com>
2024-10-19 23:17:19 +0700

    updated profile.vue data fetching process

--- pages/profile.vue
+++ pages/profile.vue
@@ -7,8 +7,8 @@
 
             <!-- Profile Picture -->
             <div class="flex flex-col items-center mb-8">
-                <img :src="profilePictureUrl || defaultAvatarUrl" alt=""
-                  class="w-24 h-24 rounded-full object-cover mb-4" />
+                <UAvatar :src="profilePictureUrl ? profilePictureUrl : USER_AVATAR_FALLBACK" alt="profile picture"
+                  size="3xl" class="object-cover mb-4 border border-gray-300 rounded-full" />
                 <button class="text-sm text-blue-500 hover:underline" @click="openFileDialog">
                     Change Profile Picture
                 </button>
@@ -16,27 +16,28 @@
             </div>
 
             <!-- Profile Information Display -->
-            <UForm :state="state" :schema="schema" class="grid gap-4">
+            <UForm ref="form" :state="state" :schema="schema" @submit.prevent="updateProfile" class="grid gap-4"
+              method="post">
                 <!-- First Name -->
-                <UFormGroup label="First Name">
+                <UFormGroup label="First Name" name="first_name">
                     <UInput v-model="state.first_name" :readonly="!editing" placeholder="First Name" />
                 </UFormGroup>
 
                 <!-- Last Name -->
-                <UFormGroup label="Last Name">
+                <UFormGroup label="Last Name" name="last_name">
                     <UInput v-model="state.last_name" :readonly="!editing" placeholder="Last Name" />
                 </UFormGroup>
 
                 <!-- Email (always read-only) -->
-                <UFormGroup label="Email">
+                <UFormGroup label="Email" name="email">
                     <UInput type="email" v-model="state.email" readonly placeholder="Email" />
                 </UFormGroup>
 
-                <UFormGroup label="Job Title">
+                <UFormGroup label="Job Title" name="job_title">
                     <UInput v-model="state.job_title" :readonly="!editing" placeholder="Job Title" />
                 </UFormGroup>
 
-                <UFormGroup label="Job Level">
+                <UFormGroup label="Job Level" name="job_level">
                     <UInput v-model="state.job_level" :readonly="!editing" placeholder="Job Level" />
                 </UFormGroup>
 
@@ -47,7 +48,7 @@
 
                 <div v-else class="flex justify-between mt-4">
                     <UButton class="w-32 py-1 text-xs flex justify-center items-center h-8" color="primary"
-                      @click="updateProfile">
+                      type="submit">
                         Save
                     </UButton>
                     <UButton class="w-28 py-1 text-xs flex justify-center items-center h-8" variant="outline"
@@ -66,10 +67,14 @@
 </template>
 
 <script setup lang="ts">
-import { onMounted, reactive, ref } from "vue";
-import { useRouter } from "vue-router";
-import { z } from "zod"
+import { reactive, ref } from "vue";
 
+import { z } from "zod"
+import type { Form } from "#ui/types";
+
+definePageMeta({
+    layout: "main-navbar"
+})
 // State
 const state = reactive<{
     first_name: string;
@@ -85,24 +90,45 @@ const state = reactive<{
     job_level: "",
 });
 
+const schema = z.object({
+    first_name: z.string().min(1, { message: "First name is required" }).max(30, { message: "First name is too long" }),
+    last_name: z.string().min(1, { message: "Last name is required" }).max(30, { message: "Last name is too long" }),
+    email: z.string().email({ message: "Invalid email address" }),
+    job_title: z.string().max(50, { message: "Job title is too long" }).nullable(),
+    job_level: z.string().max(50, { message: "Job level is too long" }).nullable(),
+})
+
+type Schema = z.infer<typeof schema>;
+const form = ref<Form<Schema>>()
+
 // Flags and refs
+const headers = useRequestHeaders(["cookie"])
 const editing = ref(false);
 const fileInput = ref();
 const selectedFile = ref();
 const profilePictureUrl = ref(""); // Reactive profile picture URL
-const defaultAvatarUrl = "https://via.placeholder.com/150"; // Fallback placeholder image
-const router = useRouter();
+const { $api } = useNuxtApp();
+const toast = useToast()
 
 // Toggle editing mode
 function toggleEdit() {
     editing.value = !editing.value;
 }
 
+const { data: profileData, refresh } = await useAPI<ApiResponse<{
+    first_name: string;
+    last_name: string;
+    email: string;
+    job_title: string;
+    job_level: string;
+    avatar_url: string;
+}>>("/user/profile-information", { headers })
+
 // Update Profile Info
 async function updateProfile() {
     try {
-        const response = await $fetch<ApiResponse>(
-            "http://localhost:8000/api/user/edit-profile",
+        const response = await $api<ApiResponse>(
+            "/user/edit-profile",
             {
                 method: "POST",
                 body: JSON.stringify({
@@ -111,24 +137,24 @@ async function updateProfile() {
                     job_title: state.job_title,
                     job_level: state.job_level,
                 }),
-                credentials: "include",
             }
         );
 
         if (response.success) {
-            alert("Profile updated successfully");
+            editing.value = false;
+            toastSuccessMessage(toast, response.message);
+            await refetch();
         } else {
-            alert(response.message || "Failed to update profile");
+            toastErrorMessage(toast, response.message);
         }
     } catch (error) {
-        console.error(error);
-        alert("An error occurred while updating your profile.");
+        toastErrorMessage(toast, "An error occurred while updating your profile.");
     }
 }
 
 // Change Password
 function changePassword() {
-    router.push("/change-password");
+    navigateTo("/change-password");
 }
 
 // Open file dialog to select new profile picture
@@ -153,58 +179,42 @@ async function uploadProfilePicture() {
     formData.append("profile", selectedFile.value);
 
     try {
-        const response = await $fetch<ApiResponse>(
-            "http://localhost:8000/api/user/upload-profile-picture",
+        const response = await $api<ApiResponse>(
+            "/user/upload-profile-picture",
             {
                 method: "POST",
                 body: formData,
-                credentials: "include",
             }
         );
 
         if (response.success) {
-            alert("Profile picture updated successfully");
+            toastSuccessMessage(toast, response.message);
+            await refetch();
         } else {
-            alert(response.message || "Failed to upload profile picture");
+            toastErrorMessage(toast, response.message);
         }
     } catch (error) {
-        console.error("Error uploading profile picture:", error);
-        alert("An error occurred while uploading your profile picture.");
+        toastErrorMessage(toast, "An error occurred while uploading your profile picture.");
     }
 }
 
-// Fetch user profile information on page load
-async function profileInformation() {
-    try {
-        const response = await $fetch<ApiResponse>(
-            "http://localhost:8000/api/auth/profile-information",
-            {
-                method: "GET",
-                credentials: "include",
-            }
-        );
-
-        if (response.success) {
-            state.first_name = response.data.first_name;
-            state.last_name = response.data.last_name;
-            state.email = response.data.email;
-            state.job_title = response.data.job_title;
-            state.job_level = response.data.job_level;
-            profilePictureUrl.value =
-                "http://localhost:8000/" + response.data.avatar_url ||
-                defaultAvatarUrl; // Set profile picture URL
-            console.log(profilePictureUrl);
-        } else {
-            alert("Failed to load profile information.");
-        }
-    } catch (error) {
-        console.error("Error fetching profile information:", error);
+function runFetchSideEffect() {
+    if (profileData.value.success && profileData.value.data) {
+        const profile = profileData.value.data;
+        state.first_name = profile.first_name;
+        state.last_name = profile.last_name;
+        state.email = profile.email;
+        state.job_title = profile.job_title;
+        state.job_level = profile.job_level;
+        profilePictureUrl.value = prefixStorageUrl(profile.avatar_url) || "";
     }
 }
 
-onMounted(() => {
-    profileInformation();
-});
+async function refetch() {
+    await refresh();
+    runFetchSideEffect();
+}
+runFetchSideEffect();
 </script>
 
 <style></style>
