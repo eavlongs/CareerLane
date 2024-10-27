3ef720260b045c7f9ddbf76c32b04d362a7c3f78
eavlongs <eavlongs@gmail.com>
2024-10-27 23:34:33 +0700

    updated check email verification middleware logic

--- middleware/check-verify.global.ts
+++ middleware/check-verify.global.ts
@@ -3,6 +3,17 @@ import { defineNuxtRouteMiddleware, navigateTo } from "nuxt/app";
 export default defineNuxtRouteMiddleware(async (to, from) => {
     const user = useUser();
 
+    if (
+        to.path.startsWith("/verify-email") &&
+        !(
+            user.value &&
+            user.value.role === UserTypeEnum.Company &&
+            !user.value.is_verified
+        )
+    ) {
+        return navigateTo("/");
+    }
+
     if (to.path.startsWith("/c/")) {
         if (!user.value) {
             return navigateTo("/login/");
